e25ed5a432ea0a43cdf782f8d4300c3cc502c9db
tejaskatkade <105963561+tejaskatkade@users.noreply.github.com>
2025-01-13 12:43:11 +0530

    MSTest project

---
+++ test/TestProject1/TestProject1/Services/CalculatorService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject1.Services
+{
+    
+        public class CalculatorService
+        {
+            public int Add(int a, int b)
+            {
+                return a + b;
+            }
+
+            public int Subtract(int a, int b)
+            {
+                return a - b;
+            }
+        }
+   
+}
---
+++ test/TestProject1/TestProject1/Test/CalculatorServiceTest.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestProject1.Services;
+
+namespace MyWebApp.Tests
+{
+    [TestClass]
+    public class CalculatorServiceTests
+    {
+        private CalculatorService _calculatorService;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _calculatorService = new CalculatorService();
+        }
+
+        [TestMethod]
+        public void Add_ShouldReturnCorrectSum()
+        {
+            // Arrange
+            int a = 5, b = 3;
+
+            // Act
+            var result = _calculatorService.Add(a, b);
+
+            // Assert
+            Assert.AreEqual(8, result, "The addition result should be 8.");
+        }
+
+        [TestMethod]
+        public void Subtract_ShouldReturnCorrectDifference()
+        {
+            // Arrange
+            int a = 5, b = 3;
+
+            // Act
+            var result = _calculatorService.Subtract(a, b);
+
+            // Assert
+            Assert.AreEqual(2, result, "The subtraction result should be 2.");
+        }
+    }
+}
